281b82a3f9bb4124be93e37b8778daf6be37bf98
huseynahmadov <t.huseyn96@mail.ru>
2024-06-26 16:14:51 +0400

    result commit result commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello develop branch. I am new user");
-        System.out.println("Hello develop branch [EDITED]  kjsdhfdsjfhds");
+        System.out.println("Hello develop branch.");
     }
 }
